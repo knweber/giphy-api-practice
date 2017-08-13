eed650118f01eca5b7d602db0c95746bd006d8eb
Kris <kris.weber.1994@gmail.com>
2017-08-13 16:44:57 -0700

    add http query to incorporate Giphy API

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
+var http = require('http');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-  res.render('home');
-})
+  console.log(req.query.term);
+  var queryString = "Ron Swanson";
+  var term = encodeURIComponent(queryString);
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+
+  http.get(url, function(response){
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      body += d;
+    });
+
+    response.on('end', function(){
+      var parsed = JSON.parse(body);
+      res.render('home', {gifs: parsed.data})
+    })
+  })
+});
 
 app.get('/yay-gif', function(req, res){
   var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
