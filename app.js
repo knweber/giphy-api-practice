e29c43bfad554635f28bb312a7c4723dcc8b3c90
Kris <kris.weber.1994@gmail.com>
2017-08-13 16:28:24 -0700

    Add route for /hello-gif

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/yay-gif', function(req, res){
-  res.send('Yay gifs!');
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl});
 });
 
 app.listen(3000,function(){
