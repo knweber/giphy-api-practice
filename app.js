209ddd3a341ddb898149d46ea8ab16e5ca2f600e
Kris <kris.weber.1994@gmail.com>
2017-08-13 16:33:58 -0700

    Add template for root page with a search form

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.get('/', function(req, res){
+  res.render('home');
+})
+
 app.get('/yay-gif', function(req, res){
   var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
   res.render('hello-gif', {gifUrl: gifUrl});
