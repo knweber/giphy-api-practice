1febcaa98d59eed5ce3ba366146a08847de26f3c
Kris <kris.weber.1994@gmail.com>
2017-08-13 17:00:02 -0700

    Add alternative implementation using giphy wrapper

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
 var http = require('http');
+var giphy = require('giphy-api')();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-  console.log(req.query.term);
-  var queryString = req.query.term;
-  var term = encodeURIComponent(queryString);
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+// IMPLEMENTATION USING HTTP
 
-  http.get(url, function(response){
-    response.setEncoding('utf8');
+// app.get('/', function(req, res){
+//   console.log(req.query.term);
+//   var queryString = req.query.term;
+//   var term = encodeURIComponent(queryString);
+//   var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+//
+//   http.get(url, function(response){
+//     response.setEncoding('utf8');
+//
+//     var body = '';
+//
+//     response.on('data', function(d) {
+//       body += d;
+//     });
+//
+//     response.on('end', function(){
+//       var parsed = JSON.parse(body);
+//       res.render('home', {gifs: parsed.data})
+//     })
+//   })
+// });
 
-    var body = '';
+// IMPLEMENTATION USING GIPHY WRAPPER
 
-    response.on('data', function(d) {
-      body += d;
-    });
-
-    response.on('end', function(){
-      var parsed = JSON.parse(body);
-      res.render('home', {gifs: parsed.data})
-    })
-  })
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  });
 });
 
 app.get('/yay-gif', function(req, res){
